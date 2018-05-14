384d0232dca372834aecab3c84fecff05f7c6f50
Alexander <ya.kotik666@yandex.ru>
2018-05-15 00:04:28 +0300

    Add animations. Prettify and finish the project

--- 1. Smart App/js/page-scroll-to-id.js
+++ 1. Smart App/js/page-scroll-to-id.js
@@ -1,16 +1,18 @@
+/**
+ * 	@description Плавный скролл после нажатия на якорную ссылку
+ */
 $(document).ready(function(){
-	/**
-	 * 	@description Плавный скролл после нажатия на якорную ссылку
-	 */
 	$("#menu").on("click","a", function (event) {
 		event.preventDefault();
 		var id  = $(this).attr('href'), top = $(id).offset().top;
 		$('body,html').animate({scrollTop: top}, 1500);
 	});
+});
 
-	/**
-	 * 	@description Изменение фона заголовочного меню при скроллинге страницы
-	 */
+/**
+ *	 @description Изменение фона заголовочного меню при скроллинге страницы
+ */
+$(document).ready(function(){
 	$(document).scroll(function() {
 		var documentScrollTop = $(document).scrollTop();
 		if (documentScrollTop > $('.header-menu').height()) {
@@ -18,5 +20,16 @@ $(document).ready(function(){
 		} else {
 			$('.header-menu').css('background-color', '#fff');
 		}
-	  });	  
+	});
+});
+
+/**
+ *	 @description Добавление анимаций из библиотеки animate.js
+ */
+$(document).ready(function(){
+	$(".animate__BounceInRight").animated("bounceInRight");
+	$(".animate__BounceInLeft").animated("bounceInLeft");
+
+	$("header i, footer i, #newsletter i").animated("bounceIn");
+	$("#features-center i").animated("pulse infinite");
 });
